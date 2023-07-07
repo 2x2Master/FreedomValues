4330158213fc8222170c98e2e9f30d80ab8ab82f
2x2Master <115193174+2x2Master@users.noreply.github.com>
2023-07-07 17:59:23 +1000

    Create ideologies.js

---
+++ ideologies.js
@@ -0,0 +1,18 @@
+ideologies = [
+    {
+        "name": "Template",
+		"desc": "insert name here",
+		"theory":"insert description here",
+        "stats": {
+            "state": 50,
+            "social": 50,
+            "econ": 50,
+            "spend": 50,
+			      "land": 50,
+			      "nation": 50,
+			      "mil": 50,
+			      "leader": 50,
+			      "appli": 50
+        }
+    }
+];
