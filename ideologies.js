2022d1c3aab2b71f064b39b73ef08fe062e6ef0b
2x2Master <115193174+2x2Master@users.noreply.github.com>
2023-07-07 18:33:14 +1000

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -8,11 +8,11 @@ ideologies = [
             "social": 50,
             "econ": 50,
             "spend": 50,
-			      "land": 50,
-			      "nation": 50,
-			      "mil": 50,
-			      "leader": 50,
-			      "appli": 50
+            "land": 50,
+            "nation": 50,
+            "mil": 50,
+            "leader": 50,
+            "appli": 50
         }
     }
 ];
