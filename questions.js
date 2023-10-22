c7c753f0c784b968a619a88560df7a7693c2c9c6
2x2Master <115193174+2x2Master@users.noreply.github.com>
2023-10-23 09:05:53 +1100

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,41 +1,106 @@
 questions = [
     {
-        "question": "Question 1",
+        "question": "The state stands in the way of liberty, and should be abolished.",
         "effect": {
             "b1": 1,
             "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Transgender rights should be a priority for any libertarian.",
+        "effect": {
+            "b1": 0,
+            "b2": 1,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "The government must intervene in the economy to prevent the formation of monopolies.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
             "b3": 1,
             "b4": 0,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "If someone is first to claim an area of land, they are entitled to profit from it.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 1,
+            "b5": 0,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Welfare spending should be cut to ensure a balanced budget.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
             "b5": 1,
+            "b6": 0,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "We shouldn't intervene in foreign conflict that don't affect our nation.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 1,
+            "b7": 0,
+            "b8": 0,
+        }
+    },
+    {
+        "question": "Direct democracy is a good goal to aim for.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
             "b6": 0,
             "b7": 1,
             "b8": 0,
         }
     },
     {
-        "question": "Question 2",
+        "question": "Violence is never an effective way to achieve political goals.",
         "effect": {
             "b1": 0,
-            "b2": 1,
+            "b2": 0,
             "b3": 0,
-            "b4": 1,
+            "b4": 0,
             "b5": 0,
-            "b6": 1,
+            "b6": 0,
             "b7": 0,
             "b8": 1,
-        }
-    },
-    {
-        "question": "Question 3",
-        "effect": {
-            "b1": -1,
-            "b2": 0,
-            "b3": -1,
-            "b4": 0,
-            "b5": -1,
-            "b6": 0,
-            "b7": -1,
-            "b8": 0,
         }
     }
 ];
